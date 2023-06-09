a4159f757b80435ba588a927b9d63cfa95fac2ed
wojciech.gradzki <gitwojciech@users.noreply.github.com>
2023-06-09 12:55:01 +0100

    [WG][WG/OPS-9649] Update libs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,14 +8,14 @@ object AppDependencies {
     val bootstrapVersion = "7.15.0"
 
     val compile: Seq[ModuleID] = Seq(
-      "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-28"         % "1.1.0",
+      "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-28"         % "1.3.0",
       "uk.gov.hmrc"         %% "bootstrap-backend-play-28"  % bootstrapVersion,
       "com.beachape"        %% "enumeratum"                 % "1.7.0",
       "com.lightbend.akka"  %% "akka-stream-alpakka-csv"    % "2.0.2"
     )
 
     val test: Seq[ModuleID] = Seq(
-      "org.scalatest"          %% "scalatest"              % "3.2.15",
+      "org.scalatest"          %% "scalatest"              % "3.2.16",
       "com.vladsch.flexmark"   %  "flexmark-all"           % "0.62.2",
       "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0",
       "org.pegdown"            %  "pegdown"                % "1.6.0",
