cd325a5dbc36edccd63be2b407c065e8faa5aef7
barnesjake <jake.barnes@digital.hmrc.gov.uk>
2026-09-02 17:28:04 +0100

    [JB][OPS-15307] run scalafmtAll and scalafmtSbt

--- project/SbtCommands.scala
+++ project/SbtCommands.scala
@@ -9,13 +9,15 @@ object SbtCommands {
       state
   }
 
-  /** This disables strict compiler options, such as wartremover, fatal warnings, and more. Use it during development you wish to postpone
-   * code cleaning.
-   */
+  /** This disables strict compiler options, such as wartremover, fatal warnings, and more. Use it during development
+    * you wish to postpone code cleaning.
+    */
   val relax: Command = Command.command(
     "relax"
   ) { state =>
-    state.globalLogging.full.info("Ok, I'll turn a blind eye to some shortcomings. Remember, I won't be so lenient on Jenkins!")
+    state.globalLogging.full.info(
+      "Ok, I'll turn a blind eye to some shortcomings. Remember, I won't be so lenient on Jenkins!"
+    )
     s"""set Global / strictBuilding := false""" ::
       state
   }
